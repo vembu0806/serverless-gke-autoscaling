565fce377a2fe2452b524f04dcacc0825c0ac685
Vembarasan Thambidurai <37688145+vembu0806@users.noreply.github.com>
2025-03-11 19:56:49 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Serverless with GKE and Cloud Run!"
+    return "Hello, Serverless!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
